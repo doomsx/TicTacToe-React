9809cc3b53cb4bd710ff1a217b0cb59983ec5a60
Doomsx <127058006+doomsx@users.noreply.github.com>
2024-08-26 20:29:04 +0800

    added winner and take turns

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,16 +3,57 @@ import Square from "./Square";
 
 function Board() {
     const [squares, setSquares] = useState(Array(9).fill(null))
+    const [isNext, setIsNext] = useState(true)
+
+    function calculateWinner(squares) {
+        const lines = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ];
+
+        for (let i = 0; i < lines.length; i++) {
+            const [a, b, c] = lines[i];
+            if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+                return squares[a];
+            }
+        }
+        return null
+    }
 
     function handleClick(i) {
+
+        if (calculateWinner(squares) || squares[i]) return
+
         const nextSquares = squares.slice();
-        nextSquares[i] = 'X';
-        console.log(nextSquares[1])
+
+        if (isNext) {
+            nextSquares[i] = 'X';
+        } else {
+            nextSquares[i] = 'O';
+        }
+
         setSquares(nextSquares);
+        setIsNext(!isNext);
+    }
+
+    const winner = calculateWinner(squares);
+    let status;
+
+    if (winner) {
+        status = 'Winner: ' + winner;
+    } else {
+        status = 'Next player: ' + (isNext ? 'X' : 'O');
     }
 
     return (
         <>
+            <h1 className="text-2xl font-bold text-center">{status}</h1>
             <div className="space-x-2">
                 <Square value={squares[0]} onSquareClick={() => { handleClick(0) }} />
                 <Square value={squares[1]} onSquareClick={() => { handleClick(1) }} />
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -3,7 +3,7 @@ function Square({ value, onSquareClick }) {
     return (
         <button className="border border-black w-[90px] h-[90px] font-bold mt-[-1px] mr-[-1px] text-3xl"
             onClick={onSquareClick}>
-            [ {value} ]
+            {value === null ? '_' : value}
         </button>
     )
 }
